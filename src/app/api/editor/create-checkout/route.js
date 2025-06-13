890ff3340d74af6341b06ab6e9d08becfdf7a704
Mikael <mikael.carlstein@gmail.com>
2025-06-13 18:04:49 +0200

    no message

--- src/app/api/editor/create-checkout/route.js
+++ src/app/api/editor/create-checkout/route.js
@@ -17,27 +17,38 @@ export async function POST(req) {
   if (!order || !Array.isArray(order.items) || order.items.length === 0) {
     return new Response(JSON.stringify({ error: 'Order not found or empty' }), { status: 404 })
   }
+
+  const totalItems = order.items.reduce((total, item) => total + item.quantity, 0)
   // Stripe line_items from order.items
   const line_items = order.items.map((item, index) => ({
     price_data: {
-      currency: 'sek',
+      currency: getCurrency({ locale }),
       product_data: {
         name: `Custom disktrasa #${index + 1}`,
         images: item.previewUrl ? [item.previewUrl] : [],
       },
-      unit_amount: calculateNetUnitAmount(39),
+      unit_amount: calculateNetUnitAmount(getPrice({ locale, totalItems })),
     },
     quantity: item.quantity || 1,
   }))
 
+  const totalPrice = line_items.reduce(
+    (total, item) => total + item.quantity * item.price_data.unit_amount,
+    0
+  )
+
+  const shippingRate = getShippingRate({ locale, totalPrice }) //totalAmount < 5000 ? SHIPPING_RATE_ID : FREE_SHIPPING_RATE_ID
+
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
+    currency: getCurrency({ locale }),
+    shipping_options: [{ shipping_rate: shippingRate }],
     payment_method_types: ['card'],
     line_items,
     customer_creation: 'always',
     billing_address_collection: 'required',
     shipping_address_collection: {
-      allowed_countries: ['SE'], // eller ['SE', 'NO', 'FI'] om du säljer till fler
+      allowed_countries: ['SE', 'NO', 'FI', 'DK'], // eller ['SE', 'NO', 'FI'] om du säljer till fler
     },
     automatic_tax: { enabled: true },
     payment_intent_data: {
@@ -62,7 +73,57 @@ export async function POST(req) {
   return Response.json({ url: session.url })
 }
 
-function calculateNetUnitAmount(grossSEK, vatPercent = 25) {
-  const net = grossSEK / (1 + vatPercent / 100)
-  return Math.round(net * 100) // Stripe needs öre
+const calculateNetUnitAmount = (grossSEK, vatPercent = 25) => {
+  return grossSEK * 100
+}
+
+const getPrice = ({ locale, totalItems }) => {
+  if (locale === 'sv') {
+    if (totalItems > 300) {
+      return 17.5
+    } else if (totalItems > 150) {
+      return 20
+    } else if (totalItems > 60) {
+      return 22.5
+    } else if (totalItems > 5) {
+      return 29
+    }
+    return 39
+  }
+
+  if (totalItems > 300) {
+    return 2
+  } else if (totalItems > 150) {
+    return 2.8
+  } else if (totalItems > 60) {
+    return 3
+  } else if (totalItems > 5) {
+    return 3.53
+  }
+  return 4
+}
+
+const getCurrency = ({ locale }) => {
+  switch (locale) {
+    case 'sv':
+      return 'sek'
+    default:
+      return 'eur'
+  }
+}
+
+const getShippingRate = ({ locale, totalPrice }) => {
+  if (locale === 'sv') {
+    if (totalPrice > 30000) {
+      return 'shr_1RZZG9P9wNW4I4PwGJXt8Zz8'
+    }
+
+    return 'shr_1RZZDmP9wNW4I4PweR3SP3JJ'
+  }
+
+  if (totalPrice > 3000) {
+    return 'shr_1RZZllP9wNW4I4PwCqKFo5w5'
+  }
+
+  return 'shr_1RZZW0P9wNW4I4PwAoCWw75d'
 }
