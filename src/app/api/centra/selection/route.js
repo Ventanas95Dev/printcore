b9d87b4f2c6303cc69f07866ea78762cc32cd6e4
Mikael <mikael.carlstein@gmail.com>
2025-06-14 16:38:09 +0200

    no message

--- src/app/api/centra/selection/route.js
+++ src/app/api/centra/selection/route.js
@@ -6,13 +6,57 @@ export async function GET(req) {
 
   console.log(location)
   return NextResponse.json({
-    location: {
-      country: 'SE',
-      name: 'Sweden',
-      shipTo: true,
-      market: 1,
-      pricelist: 2,
-      language: 'sv',
-    },
+    location: { ...getRightLocation(location?.country) },
   })
 }
+
+const getRightLocation = (country) => {
+  if (country === 'SE') {
+    return { country: 'SE', shipTo: true, market: 1, pricelist: 2, language: 'sv' }
+  }
+
+  const isEEA = isEEACountry(country) // true
+  if (isEEA) {
+    return { country: country, shipTo: true, market: 1, pricelist: 1, language: 'sv' }
+  }
+
+  return { country: country, shipTo: true, market: 1, pricelist: 5, language: 'sv' }
+}
+
+function isEEACountry(countryCode) {
+  return EEA_COUNTRIES.has(countryCode?.toUpperCase())
+}
+const EEA_COUNTRIES = new Set([
+  // EU countries
+  'AT',
+  'BE',
+  'BG',
+  'HR',
+  'CY',
+  'CZ',
+  'DK',
+  'EE',
+  'FI',
+  'FR',
+  'DE',
+  'GR',
+  'HU',
+  'IE',
+  'IT',
+  'LV',
+  'LT',
+  'LU',
+  'MT',
+  'NL',
+  'PL',
+  'PT',
+  'RO',
+  'SK',
+  'SI',
+  'ES',
+  'SE',
+  // EEA but not EU
+  'IS',
+  'LI',
+  'NO',
+])
